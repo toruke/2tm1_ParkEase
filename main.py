c51f3f053973c8e6f7b13d967ea0346cd18c9bcf
Clément <c.vier@students.ephec.be>
2024-10-30 14:54:28 +0100

    ajout des paramètres

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
 from libs import parking
+import argparse
+
+def main(args):
+    pass
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Parking manager')
+    parser.add_argument('-p', '--plate', type=str, help='Plate number', required=True)
+    parser.add_argument('-s', '--state', type=str, choices=['in', 'out'], help='Status of the car: in/out', required=True)
+    args = parser.parse_args()
+
+    main(args)
